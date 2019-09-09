5c3cbc6d606569e40f35cbd1b85510d9acb9576f
ViperART <viperart@yandex.ru>
2019-09-09 04:59:40 +0300

    Added sticky navigation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,25 @@
+// TABS CHANGER
+
+function changeDay(event, dayNumber) {
+
+    let tabcontent = document.getElementsByClassName("agenda__schedule");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    
+    let tablinks = document.querySelectorAll(".agenda__select");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove('agenda__select--selected');
+    }
+    
+    document.getElementById(dayNumber).style.display = "grid";
+    event.currentTarget.classList.add('agenda__select--selected');
+}
+
+
+// TIMER
+
 function getTimeRemaining(endtime) {
     let ms = Date.parse(endtime) - Date.parse(new Date());
     let seconds = Math.floor((ms / 1000) % 60);
@@ -44,5 +66,31 @@ function startClock(endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-const deadline = '2019-12-31';
-startClock(deadline);
+
+// STICKY HEADER
+
+function applyStickyNavigation() {
+    let waypoint = new Waypoint({
+        element: $('.explore'),
+        handler: function(direction) {
+            if (direction === 'down') {
+                $('.header').addClass('header--sticky');
+                console.log('123');
+            } else {
+                $('.header').removeClass('header--sticky');
+            }
+        }
+      })
+}
+
+// START
+
+$(document).ready(function() {
+    document.getElementById("default").click(); // Select default tab
+
+    // Set deadline and start timer
+    const deadline = '2019-12-31';
+    startClock(deadline);
+
+    applyStickyNavigation();
+})
